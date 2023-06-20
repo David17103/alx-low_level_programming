137e9e7f9701d25437c7368c83ca02658c95c548
David17103 <davida.chetty12@gmail.com>
2023-06-19 23:18:56 -0700

    Task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC *.c -shared -o liball.so
+
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+
+for file in $c_files; do
+    gcc -c -fPIC "$file" -o "${file%.c}.o"
+done
+
+gcc -shared -o liball.so *.o
+
+rm -f *.o
+
