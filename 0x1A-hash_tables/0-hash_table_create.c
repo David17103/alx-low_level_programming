959dfdd47c933fca9ee0e0fc64b09c08d22ec8ad
David17103 <davida.chetty12@gmail.com>
2023-09-08 12:04:57 -0700

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  * Return: A pointer to the newly created hash table, or NULL if allocation fails.
  */
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
     unsigned int i = 0;
     hash_table_t *ht = malloc(sizeof(hash_table_t));
