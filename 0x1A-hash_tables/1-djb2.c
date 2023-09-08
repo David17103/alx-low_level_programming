82bdb23554b4849e47bd8f6b3a299855922440c6
David17103 <davida.chetty12@gmail.com>
2023-09-08 12:01:29 -0700

    1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,10 @@
 /**
- * custom_hash - Custom hash function that transforms a string into a hash value.
+ * hash_djb2 - Custom hash function that transforms a string into a hash value.
  * @str: The input string, which should be a unique value within the hash table.
  *
  * Return: The resulting hash value.
  */
-unsigned long int custom_hash(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
     int c;
